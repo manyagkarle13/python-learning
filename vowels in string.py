252f69033bf7f6b884f493ecd38d852debe169da
Manya G Karle <manyagkarle@gmail.com>
2025-09-15 20:01:03 +0530

    Add vowel counting script

---
+++ vowels in string.py
@@ -0,0 +1,8 @@
+#Count how many vowels are in a given string.
+string=input("Enter a string: ")
+vowels="aeiouAEIOU" 
+count=0
+for char in string:
+    if char in vowels:
+        count+=1    
+print("Number of vowels in the string:", count)
